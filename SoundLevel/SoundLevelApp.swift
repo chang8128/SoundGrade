a001bd25db4611125ddbc09e1f7169c76b3bf803
chang8128 <chang8128@sina.cn>
2022-11-29 18:41:03 +0800

    Initial Commit

---
+++ SoundLevel/SoundLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundLevelApp.swift
+//  SoundLevel
+//
+//  Created by 常志平 on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SoundLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
