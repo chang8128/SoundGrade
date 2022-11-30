f2b1e33ad5013a756202850fff02882c3f6a28a8
chang8128 <chang8128@sina.cn>
2022-11-30 15:17:54 +0800

    some achievement

--- SoundLevel/ContentView.swift
+++ SoundLevel/ContentView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columnLayout = Array(repeating: GridItem(), count: 4)
+    
+    let allColors: [Color] = [
+        .pink,
+        .red,
+        .orange,
+        .yellow,
+        .mint,
+        .teal,
+        .cyan,
+        .blue,
+        .indigo,
+        .purple,
+        .brown,
+        .gray
+    ]
+    
+    
     var body: some View {
-        HStack {
-            Button(action: {
-                print("did tap...")
-            }, label: {Text("1")})
-            
-            Button(action: {
-                print("did tap...")
-            }, label: {Text("2")})
-            
-            Button(action: {
-                print("did tap...")
-            }, label: {Text("3")})
+        VStack {
+            LazyVGrid(columns: columnLayout) {
+                ForEach(allColors, id: \.self) { color in
+                    Button {
+                        print("did something")
+                    } label: {
+                        ZStack {
+                            Text("Test of \(color.description)")
+                            RoundedRectangle(cornerRadius: 30.0)
+                                .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                .foregroundColor(color)
+                            .opacity(0.5)
+                        }
+                    }
+                }
+            }
         }
         .padding()
     }
---
+++ SoundLevel/TestNumbersView.swift
@@ -0,0 +1,56 @@
+//
+//  TestNumbersView.swift
+//  SoundLevel
+//
+//  Created by 常志平 on 30/11/22.
+//
+
+import SwiftUI
+
+struct TestNumbersView: View {
+    let columnLayoutA = Array(repeating: GridItem(), count: 4)
+    
+    let allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    
+    let allColors: [Color] = [
+        .pink,
+        .red,
+        .orange,
+        .yellow,
+        .mint,
+        .teal,
+        .cyan,
+        .blue,
+        .indigo,
+        .purple,
+        .brown,
+        .gray
+    ]
+    
+    var body: some View {
+        
+        ScrollView {
+            LazyVGrid(columns: columnLayoutA) {
+                ForEach(allNumbers, id: \.self) {
+                    Text("\($0)")
+                        .font(.largeTitle)
+                }
+            }
+            .padding(.vertical)
+            
+            VStack(alignment: .leading) {
+                ForEach(allColors, id: \.self) { color in
+                    Text("this color is: \(color.description)")
+                }
+            }
+        }
+        
+        
+    }
+}
+
+struct TestNumbersView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestNumbersView()
+    }
+}
